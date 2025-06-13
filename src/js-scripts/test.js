846a26e33f97d1d950f806f3649bf103a2a14a33
Kushal7788 <kushshah777888@gmail.com>
2025-06-13 14:57:19 +0530

    Updated sample js scripts

--- src/js-scripts/test.js
+++
@@ -1,5 +0,0 @@
-setInterval(() => {
-    if (window.location.href === "https://all.accor.com/loyalty-funnel/en/status" || window.location.href === "https://all.accor.com/loyalty-funnel/en/status/") {
-        window.location.href = "https://all.accor.com/a/en/deals-corner.html";
-    }
-}, 500);
